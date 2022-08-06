9661763830f5fd0d5c6a362fec7b9b9fb7ef4846
ShinjiOka <oshinji871@gmail.com>
2022-08-06 13:04:08 +0900

    Create form and table views

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 function App() {
   return (
     <div>
-      <h1>Todoアプリ</h1>
+      <h1>簡易Todoアプリ</h1>
+      <form action="">
+        <label htmlFor="">タイトル：</label>
+        <input type="text" />
+        <input type="submit" value="作成" />
+      </form>
+      <hr />
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>タイトル</th>
+            <th>進捗
+              <input type="checkbox" />
+            </th>
+          </tr>
+        </thead>
+      </table>
     </div>
   )
 }
